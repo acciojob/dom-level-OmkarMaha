8298b5bd65db4c6193f9ac4037de66214b056544
OmkarMaha <161202198+OmkarMaha@users.noreply.github.com>
2024-07-07 12:19:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function findDomElement(elementId) {
+	var element  = document.getElementById(elementId)
+	var level = 0
+
+	while (element) {
+		level++;
+		element = element.parentElement;
+	}
+    console.log(level)
+	return level
+}
+
+var domLevel = findDomElement('level')
+
+alert('The level of the element is: ' + domLevel);
